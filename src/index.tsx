5489dce99d9fb6194cc0e47a1b7c4881a821b120
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2023-12-20 19:01:07 +0200

    Initialize Firebase
    Add the Firebase config object to the index.tsx component.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RouterComponent from './components/Router';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDd6Q5TJeR0Wa1KtLXq9-DUbZhMGkIiqC4",
+  authDomain: "chatzilla-8a1f8.firebaseapp.com",
+  projectId: "chatzilla-8a1f8",
+  storageBucket: "chatzilla-8a1f8.appspot.com",
+  messagingSenderId: "76532236145",
+  appId: "1:76532236145:web:546fd0a24931a04967c0d0",
+  measurementId: "G-XZ84VZJD8V"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
