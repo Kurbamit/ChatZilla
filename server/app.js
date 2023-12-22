0ccd8407703b45ab87559fe6449d8ed8fc007a92
rytiskar <rytis.karalius321@gmail.com>
2023-12-22 18:50:35 +0200

    Add functionality for handling http errors

--- server/app.js
+++ server/app.js
@@ -2,7 +2,25 @@ const express = require('express');
 const app = express();
 
 const usersRoutes = require('./api/routes/usersRoutes');
+const { error } = require('console');
 
 app.use('/users', usersRoutes)
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    // forward error request for next 
+    next(error);
+});
+
+// Handles all kind of errors being thrown
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;   
