a654428d68d6492a7dc223029983264f390d8665
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2023-12-23 21:16:40 +0200

    Add the Login

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,41 +1,66 @@
-import React from 'react';
+import React from "react";
 import "../styles/style.css";
+import { useLoginForm } from "../scripts/loginUtils/LogInUtils";
 
 function Login() {
-    return (
-        <>
-            <div className = 'login-box'>
-                <h2>LOGIN</h2>
-                <form>
-                    <div className= 'input-field'>
-                        <input type='text' name='' required></input>
-                        <label>Email or username</label>
-                    </div>
-                    <div className='input-field'>
-                        <input type='password' name='' required></input>
-                        <label>Password</label>
-                    </div>
-                    <a href="#" className='btn mt-4'>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        Sign in
-                    </a>
-                    <a href="/Signup" className='btn mt-4'>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        Sign up
-                    </a>
-                    <p className='mb-0 mt-4 text-center'>
-                        <a href="#0" className='link'>Forgot your password?</a>
-                    </p>
-                </form>
-            </div>
-        </>
-    );
+  const { loginInfo, emailValidation, handleInputChange, handleLogIn } =
+    useLoginForm();
+
+  return (
+    <div className="login-box">
+      <h2>LOGIN</h2>
+      <form>
+        <div className="input-field">
+          <input
+            type="text"
+            name="email"
+            value={loginInfo.email}
+            onChange={handleInputChange}
+            style={{
+              borderBottom: `1px solid ${
+                emailValidation.isValid ? "green" : "red"
+              }`,
+              color: emailValidation.isValid ? "green" : "red",
+            }}
+            required
+          ></input>
+          <label>Email</label>
+        </div>
+        <div className="input-field">
+          <input
+            type="password"
+            name="password"
+            value={loginInfo.password}
+            onChange={handleInputChange}
+            style={{
+              borderBottom: "1px solid black", // Customize the styling for the password field
+            }}
+            required
+          ></input>
+          <label>Password</label>
+        </div>
+        <button className="btn mt-4" onClick={handleLogIn}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Log in
+        </button>
+        <a href="/Signup" className="btn mt-4">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Sign up
+        </a>
+        <p className="mb-0 mt-4 text-center">
+          <a href="#0" className="link">
+            Forgot your password?
+          </a>
+        </p>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- client/src/components/Router.tsx
+++ client/src/components/Router.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from './Login';
+import Login from "./Login";
 import Signup from "./Signup";
 
 const RouterComponent: React.FC = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login/>}/>
-                <Route path="/Signup" element={<Signup/>}/>
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default RouterComponent;
